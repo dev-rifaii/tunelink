aed51ccd7938090adff2c270ceca7e841fad32c2
Mohammad <moe.rifaii77@gmail.com>
2022-06-01 14:09:48 +0300

    add global cors config

--- src/main/java/perosnal/spotifymatcher/WebConfig.java
+++ src/main/java/perosnal/spotifymatcher/WebConfig.java
@@ -1,5 +1,7 @@
 package perosnal.spotifymatcher;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,11 +9,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+@Slf4j
+class WebConfig implements WebMvcConfigurer {
+
+    private final String activeProfiles;
+
+    WebConfig(@Value("${spring.profiles.active}") final String activeProfiles) {
+        this.activeProfiles = activeProfiles;
+    }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    public void addCorsMappings(final CorsRegistry registry) {
+        if ("local".equals(activeProfiles)) {
+            registry.addMapping("/**");
+        } else if ("heroku".equals(activeProfiles)) {
+            log.info("ACTIVE PROFILE IS HEROKU");
+            registry.addMapping("/**")
+                    .allowedOrigins("https://dev-rifaii.github.io/");
+        } else {
+            throw new IllegalStateException("invalid profile " + activeProfiles);
+        }
     }
 
 }
